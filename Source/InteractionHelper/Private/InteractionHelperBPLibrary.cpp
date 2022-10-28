b175c9a4de187d816b94593c04389ef979eef2f9
zld <zld@wooduan.com>
2022-10-28 11:28:19 +0800

    Init

---
+++ Source/InteractionHelper/Private/InteractionHelperBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InteractionHelperBPLibrary.h"
+#include "InteractionHelper.h"
+
+UInteractionHelperBPLibrary::UInteractionHelperBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UInteractionHelperBPLibrary::InteractionHelperSampleFunction(float Param)
+{
+	return -1;
+}
+
